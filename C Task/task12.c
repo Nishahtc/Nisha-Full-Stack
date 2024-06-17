3062b0cea7f6ab5e0904eb17698768d3f8cfdddc
Nishahtc <nishamodi254@email.com>
2024-06-17 16:14:59 +0530

    revers

---
+++ C Task/task12.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char name[10];
+    char count=0;
+    printf("enter name");
+    scanf("%s",&name);
+    for(int x= strlen(name); x>=0; x--)
+    {
+       printf("%c",name[x]);
+    }
+    return 0;
+}
